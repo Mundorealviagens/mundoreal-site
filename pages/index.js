7a31f6d7198a07473f295f339721bc9bbb1b32d7
Mundorealviagens <contato@mundoreal.tur.br>
2025-04-02 14:41:15 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>🌎 Bem-vindo à Mundo Real Viagens</h1>
+      <p>Seu site está online! Personalize o conteúdo editando as páginas no diretório <code>pages/</code>.</p>
+    </div>
+  );
+}
